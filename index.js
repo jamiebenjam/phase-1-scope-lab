9422a767e3ce582709ce071d8d4c37360f42f1aa
Jamie Benjamin <bennythedev@gmail.com>
2022-06-08 09:37:53 -0500

    commit tree e2c85c7a93f3fd290b549c683e25b9705f1aecbd

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = 'initial value';
+
+// function callCustomerName() {
+//     return customerName;
+// }
+
+// callCustomerName();
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "new value";
+}
